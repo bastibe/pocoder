7ddf592f0c8f20310e27bcadf151ea594e554adb
Francis <iglusdeneon@gmail.com>
2018-12-12 13:37:27 +0100

    fixed some pep8 changes

--- pocoder.py
+++ pocoder.py
@@ -6,6 +6,7 @@ from numpy import correlate, dot, append, divide, argmax, int16, sqrt, power
 from numpy.random import randn
 from matplotlib.pyplot import plot, figure, show
 
+
 def block_process(data, fs, block_len, overlap):
     """
     A generator that slices an input array into overlapping blocks.
@@ -18,40 +19,43 @@ def block_process(data, fs, block_len, overlap):
     block_samples = round(block_len * fs)
     overlap_samples = round(block_samples * overlap)
     shift_samples = block_samples - overlap_samples
-    num_blocks = floor((len(data)-overlap_samples) / shift_samples)
+    num_blocks = floor((len(data) - overlap_samples) / shift_samples)
     for idx in arange(0, num_blocks):
-        samples = data[idx*shift_samples:idx*shift_samples+block_samples]
-        yield(array(samples, copy=True), idx*shift_samples)
+        samples = data[idx * shift_samples:idx * shift_samples + block_samples]
+        yield (array(samples, copy=True), idx * shift_samples)
+
 
 def levinson_algorithm(r):
     """
     Calculates A and K coefficients of an auto correlation array using
     the levinson-durben method.
     """
-    a = zeros(len(r));
-    k = zeros(len(r));
-    for m in arange(0,len(r)-1):
-        alpha = -dot( r[m::-1], append(a[m:0:-1], 1.0) )
-        mu = -dot(r[m::-1], append(a[1:m+1], 0.0)) - r[m+1]
+    a = zeros(len(r))
+    k = zeros(len(r))
+    for m in arange(0, len(r) - 1):
+        alpha = -dot(r[m::-1], append(a[m:0:-1], 1.0))
+        mu = -dot(r[m::-1], append(a[1:m + 1], 0.0)) - r[m + 1]
         k[m] = -mu / alpha
-        a[1:m+2] = append(a[1:m+1], 0.0) + k[m] * append(a[m:0:-1], 1.0)
+        a[1:m + 2] = append(a[1:m + 1], 0.0) + k[m] * append(a[m:0:-1], 1.0)
 
     a[0] = 1
-    return (a,k)
+    return (a, k)
 
-def fundamental_period_estimate(rxx,fs):
+
+def fundamental_period_estimate(rxx, fs):
     """
     Calculates the fundamental frequency of an auto correlation array.
 
     rxx   the auto correlation array.
     fs    the sample rate in hertz.
     """
-    f_low, f_high = 50 , 250
+    f_low, f_high = 50, 250
     f_low_idx = round(fs / f_low)
     f_high_idx = round(fs / f_high)
-    period_idx = argmax(rxx[f_high_idx:f_low_idx ]) + f_high_idx
+    period_idx = argmax(rxx[f_high_idx:f_low_idx]) + f_high_idx
     is_voiced = max(rxx) > 0.20
-    return(period_idx, is_voiced)
+    return (period_idx, is_voiced)
+
 
 def vocode(signal, fs, block_len, overlap, order):
     """
@@ -70,22 +74,22 @@ def vocode(signal, fs, block_len, overlap, order):
     returns a vocoded signal of the same sample rate as the original.
     """
 
-    b, a = butter(1, 200/fs, 'high')
+    b, a = butter(1, 200 / fs, 'high')
     glottal_lowpass = lambda signal: lfilter(b, a, signal)
 
     out = zeros(len(signal))
     for block, idx in block_process(signal, fs, block_len, overlap):
-        gain_correction = (1-overlap) * 2 # *2 due to hann window
+        gain_correction = (1 - overlap) * 2  # *2 due to hann window
         block *= hann(len(block)) * gain_correction
 
         rxx = correlate(block, block, mode='full')
-        rxx = rxx[len(rxx)/2:]
+        rxx = rxx[len(rxx) / 2:]
         period_samples, is_voiced = fundamental_period_estimate(rxx, fs)
 
         block = preemphasis(block)
         rxx = correlate(block, block, mode='full')
-        rxx = rxx[len(rxx)/2:]
-        a, k = levinson_algorithm(rxx[:order+1])
+        rxx = rxx[len(rxx) / 2:]
+        a, k = levinson_algorithm(rxx[:order + 1])
         error_power = rms(lfilter(a, (1,), block))
 
         if is_voiced:
@@ -93,28 +97,32 @@ def vocode(signal, fs, block_len, overlap, order):
             vocoded[period_samples::period_samples] = 1.0
             vocoded = glottal_lowpass(vocoded)
         else:
-            vocoded = randn(len(block))/2
+            vocoded = randn(len(block)) / 2
 
         vocoded = lfilter((error_power,), a, vocoded)
         vocoded *= hann(len(block))
-        out[idx:idx+len(block)] += deemphasis(vocoded)
+        out[idx:idx + len(block)] += deemphasis(vocoded)
     return out
+
 
 def preemphasis(signal):
     return lfilter([1, -0.70], 1, signal)
 
+
 def deemphasis(signal):
     return lfilter([1, 0.70], 1, signal)
 
+
 def rms(signal):
     return sqrt(mean(power(signal, 2)))
+
 
 if __name__ == "__main__":
     fs, data = wavread('Mann.wav')
     data = array(data, dtype=double)
     data /= amax(absolute(data))
     data = decimate(data, 4)
-    fs = round(fs/4)
+    fs = round(fs / 4)
 
     block_len = 0.032
     overlap = 0.5
@@ -122,7 +130,7 @@ if __name__ == "__main__":
 
     out = vocode(data, fs, block_len, overlap, order)
 
-    wavwrite('vocoded.wav', fs, array(out/amax(absolute(out)) * (2**15-1), dtype=int16))
+    wavwrite('vocoded.wav', fs, array(out / amax(absolute(out)) * (2 ** 15 - 1), dtype=int16))
 
     figure()
     plot(data)
